6a744671ad947695d87f4a02ff75ed39f84068a5
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-05 02:04:56 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "283039ab-8c2c-4e59-b27c-c308e6da2438",
+    "home_feed": [
+        {
+            "id": "9de2104e-b1c2-4135-9652-e6e3b0c1275a",
+            "artist_name": "poor",
+            "home_carousel": [
+                {
+                    "id": "c7a0f079-6857-4dd2-9896-34f9cafec4ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png"
+                },
+                {
+                    "id": "63254e31-dae5-46f5-b351-9815c28ef912",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "8b7b2ed3-8b36-45a6-bd95-97f455ac63aa",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/136.png"
+                },
+                {
+                    "id": "c3100d5a-6e85-4535-833d-486785117246",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/113.png"
+                },
+                {
+                    "id": "6d280670-bc24-4526-a00c-d5e19b6dc131",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                }
+            ],
+            "art_caption": "training",
+            "art_description": "Maybe increase party race activity interesting. Real dark hotel remember hour common. Network child unit for raise environmental too our."
+        },
+        {
+            "id": "c92ca155-857b-4c63-8b8a-a1bb2717a084",
+            "artist_name": "or",
+            "home_carousel": [
+                {
+                    "id": "b09e2de3-3a13-4314-b598-93ac55230c30",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png"
+                },
+                {
+                    "id": "c76f7876-9289-4c72-b55f-da472d04da3b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png"
+                },
+                {
+                    "id": "2539ad6a-923b-4226-98b5-5ac4dec9f121",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "1b4c39e9-0860-4df3-bb4f-608bc1dd121e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "fa6d1ab0-14cc-410f-8942-982e65c4a02a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/166.png"
+                }
+            ],
+            "art_caption": "service",
+            "art_description": "Country lawyer care support impact agent. Close back serious red eat Mr industry."
+        },
+        {
+            "id": "2de3b9df-ad2e-484b-961b-150a3ad764aa",
+            "artist_name": "important",
+            "home_carousel": [
+                {
+                    "id": "0bfb3b20-ca4f-419f-b9ea-3c6e1b78b50d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/192.png"
+                },
+                {
+                    "id": "3e4039c7-999e-46a4-8d69-9b6e3270a5f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "62268431-3f7d-403e-a7e3-77fdd58b8362",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/3.png"
+                },
+                {
+                    "id": "5290f497-6360-4542-9444-6e5a5ed2343f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png"
+                },
+                {
+                    "id": "af53bad4-c80a-4af8-98e0-89bd30adf13f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/301.png"
+                }
+            ],
+            "art_caption": "far",
+            "art_description": "Point alone themselves prove clearly know democratic. Government let car up."
+        },
+        {
+            "id": "36ac0dbb-30af-4457-98a7-2e640fe1512f",
+            "artist_name": "than",
+            "home_carousel": [
+                {
+                    "id": "50db3828-c603-44eb-989b-f9604e7d791e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                },
+                {
+                    "id": "9b9aeec0-c1c4-450e-988c-3c5ab1e2bd63",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/132.png"
+                },
+                {
+                    "id": "2bbacb95-2c98-42e9-9b89-519560d8d0ce",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "3f609fbe-6d20-4ff1-8eba-a61d764e27e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/70.png"
+                },
+                {
+                    "id": "fca1bd60-e6a6-4db9-928f-af9f29b2d0dc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/99.png"
+                }
+            ],
+            "art_caption": "factor",
+            "art_description": "Property instead edge but. Light try life treatment."
+        },
+        {
+            "id": "b7a8417e-abec-45a3-949b-f692e4fdf69b",
+            "artist_name": "control",
+            "home_carousel": [
+                {
+                    "id": "4b9fd123-f5c3-4258-817c-b80cd7f04ee9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "b4ddaef8-5c00-4d95-be6e-f9dce32434ba",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                },
+                {
+                    "id": "7b996d5c-c659-4ecf-8fe3-1b98885ca9e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/123.png"
+                },
+                {
+                    "id": "e270f471-2a40-465c-9718-2687ae863a00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/226.png"
+                },
+                {
+                    "id": "4a25ec3f-3c5a-484c-816e-1b8510c06f0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                }
+            ],
+            "art_caption": "main",
+            "art_description": "Risk name in. Defense growth start true. Again identify risk than case peace itself. Budget house event."
+        }
+    ]
+}
